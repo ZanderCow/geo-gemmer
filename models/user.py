41512f288100af0eb037990fb5864c3559519ab2
JusPSLiu <juspsliu@yahoo.com>
2024-03-28 00:18:19 -0400

    fixed the tests

--- models/user.py
+++ models/user.py
@@ -1,12 +1,22 @@
 class User:
     """A user holds personal information and interactions with gems"""
-   
-    def __init__(self, username: str, first_name: str, last_name: str, password: str, profile_picture: str, gems_explored: int, reviews_made: int, gems_created: int, gems_saved_count: int) -> None:
+    username = "None"
+    id = 1
+    first_name = "None"
+    last_name = "None"
+    password = "None"
+    profile_picture = "None"
+    gems_explored = 0
+    reviews_made = 0
+    gems_created = 0
+    gems_saved_count = 0 
+    def __init__(self, username, new_id, first_name, last_name, password, profile_picture, gems_explored, reviews_made, gems_created, gems_saved_count):
         """
         Initialize a User object with the provided information 
 
         Args:
             username (str): The username of the user.
+            id (int): The ID of the user
             first_name (str): The first name of the user.
             last_name (str): The last name of the user.
             password (str): The password of the user.
@@ -19,4 +29,13 @@ class User:
         Returns:
             None
         """
-        
+        self.username = username
+        self.id = new_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.profile_picture = profile_picture
+        self.gems_explored = gems_explored
+        self.reviews_made = reviews_made
+        self.gems_created = gems_created
+        self.gems_saved_count = gems_saved_count        
