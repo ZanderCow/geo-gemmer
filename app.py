9f3be4dfe5cc7eb8265ced15ea24f51cf10cb524
Zander Cowan <128069231+ZanderCow@users.noreply.github.com>
2024-03-27 22:26:58 -0400

    refactored UI and added animations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, redirect
+from flask import Flask,render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -31,10 +31,10 @@ def signup_user():
     return redirect('/dashboard')
 
 
-@app.get('/login')
+@app.get('/sign-in')
 def get_login():
     # gets login page
-    return render_template('login.html')
+    return render_template('sign-in.html')
 
 @app.post('/login')
 def login_user():
@@ -53,72 +53,8 @@ def dashboard():
     The rendered user main menu page.
    '''
     
-    user_data_example = {
-        "username": "TheCowanPlayz",
-        "first_name": "Zander",
-        "last_name": "Cowan",
-        "profile_picture": "../static/img/nature-image.png",
-        "gems_explored": 5,
-        "reviews_made": 0,
-        "gems_created": 0,
-        "gems_saved": 4,
-       
-        "gems_visited": [
-            {
-                "gem_name": "Rocky Mountain",
-                "gem_type": "mountain",
-                "date_visited": "2023-03-10",
-            },
-            {
-                "gem_name": "slipper slope",
-                "gem_type": "bar",
-                "date_visited": "2023-04-10",
-            },
-            {
-                "gem_name": "Diamond Lake",
-                "gem_type": "lake",
-                "date_visited": "2023-05-15"
-            },
-            {
-                "gem_name": "Emerald Forest",
-                "gem_type": "forest",
-                "date_visited": "2023-06-20"
-            },
-            {
-                "gem_name": "Sapphire Falls",
-                "gem_type": "waterfall",
-                "date_visited": "2023-07-25"
-            }
-        ],
-
-
-        "gems_saved_details": [
-            {
-                "gem_name": "Rocky Mountain",
-                "gem_type": "mountain",
-                "website_link": "https://rockymountain.com",
-            },
-            {
-                "gem_name": "slipper slope",
-                "gem_type": "bar",
-                "website_link": "https://slipperslope.com",
-            }
-        ],
-                 
-        "left_reviews": [
-            {
-                "rating": 4,
-                "review": "this place is great as;lfkjsda;lfkas"
-            },
-            {
-                "rating": 1,
-                "review": "this place sucks fsdajk;lfsadjfklsda"
-            }
-        ]
-
-
-    }
-    return render_template('user-main-menu.html', user_data=user_data_example)
+   
+    return render_template('user-main-menu.html')
 
 @app.get('/user-settings')
 def userprofile():
@@ -129,15 +65,8 @@ def userprofile():
         str: The rendered user settings HTML page with the user profile data.
     """
 
-    user_settings_example = {
-        "username": "TheCowanPlayz",
-        "first_name": "Zander",
-        "last_name": "Cowan",
-        "email": "zandercowan01@gmail.com",
-        "profile_picture": "../static/img/nature-image.png",
-    }
 
-    return render_template('user-settings.html', profile_data=user_settings_example)
+    return render_template('user-settings.html')
 
 @app.post('/user-settings')
 def update_userprofile():
@@ -152,15 +81,11 @@ def update_userprofile():
 
 
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.get('/search')
 def search():
-    #change this to a search query
-    gem_search_example = {
-
-
-
-    }
     return render_template('gem-search.html')
+
+
 
 '''
 this is for later. to when we need to setup the search query correctly 
@@ -172,36 +97,7 @@ def search():
 
 @app.route('/gem-details', methods=['GET', 'POST'])
 def gemdetails():
-
-    gem_data_example = {
-        "name": "Rocky Mountain",
-        "gem_type": "mountain",
-        "times_visited": 2035,
-        "acessibilty": "elderly friendly, wheelchair unaccessible",
-        "website_link": "http://rockymountain.com",
-
-        "gem_images":{
-            "image_1": "../static/img/nature-image.png",
-            "image_2": "../static/img/nature-image.png",
-            "image_3": "../static/img/Screenshot%202024-03-10%20at%203.40.09 PM.png",
-
-
-        },              
-        "reviews": [
-            {
-                "username": "gertrude123",
-                "rating": 4,
-                "review": "this place is great as;lfkjsda;lfkas"
-            },
-            {
-                "username": "suzansmith",
-                "rating": 1,
-                "review": "this place sucks fsdajk;lfsadjfklsda"
-            }
-        ]
-    }
-
-    return render_template('gem-details.html', gem_data=gem_data_example)
+    return render_template('gem-details.html')
 
 '''
 this is for later, when we setup the search query correctly
