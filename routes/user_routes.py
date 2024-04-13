4405231dfeb0c1a8ac8b33049d2d50859414090a
Zander Cowan <128069231+ZanderCow@users.noreply.github.com>
2024-04-13 14:08:41 -0400

    added backend valiation for the create gem page

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template,redirect,request
+from flask import Blueprint, render_template,redirect,request,jsonify
 
 user = Blueprint('user', __name__)
 
@@ -109,7 +109,26 @@ def render_create_gem_page():
 
 @user.post('/create-gem')
 def create_gem():
-    #TODO:
-    # create gem in database
-    return render_template('about.html')
+    """
+    Create a new gem in the database based on the incoming JSON data.
+
+    Returns:
+        A JSON response containing a success message and the URL of the created gem.
+    """
+    data = request.get_json()  # get the incoming JSON data
+
+    # list of fields that should not be empty
+    required_fields = ['gem_name', 'gem_type', 'latitude', 'longitude']
+
+    missing_fields = [field for field in required_fields if not data.get(field)]
+
+    if missing_fields:
+        return jsonify({'error': f'The following fields are required and were not provided: {", ".join(missing_fields)}'}), 400  # 400 is the status code for "Bad Request"
+
+    return jsonify(
+    {
+        'message': 'Gem created successfully',
+        'gem_url': '/gem/67e55044-10b1-426f-9247-bb680e5fe0c8'
+    }
+)
 
