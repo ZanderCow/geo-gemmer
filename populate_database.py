e9b1631bfa1cac6f24bf27775f20ce25f52ce32a
JusPSLiu <juspsliu@yahoo.com>
2024-04-21 05:23:14 -0400

    began to connect gems to frontend

---
+++ populate_database.py
@@ -0,0 +1,4 @@
+from repositories import gem_repository as gem_repo, user_repository as user_repo
+
+gem_repo.create_new_gem("University of North Carolina at Charlotte", "Zoo", -80.843124, 35.227085, False, 'https://bloximages.newyork1.vip.townnews.com/greensboro.com/content/tncms/assets/v3/editorial/2/aa/2aaa569b-c6f1-5364-bfc8-f0e6da2505e0/5c86c9854a46c.image.jpg', '/static/img/grandma.png', '/static/img/neckbeard.png')
+gem_repo.create_new_gem("northy north trail", "Hiking Trail", -80.443124, 35.127085, False, '/static/img/nature-image.png', '/static/img/grandma.png', '/static/img/neckbeard.png')
