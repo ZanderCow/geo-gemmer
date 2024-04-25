31065d287fc6c2dcac977a001a5205a8d426790f
JusPSLiu <juspsliu@yahoo.com>
2024-04-25 13:40:50 -0400

    updated search functionality

--- repositories/db.py
+++ repositories/db.py
@@ -14,7 +14,6 @@ def get_pool():
 
 def inflate_string(input:str, max_length:int|None=None) -> str:
     '''
-
     Put string in PostgreSQL form in case of apostrophe catastrophe
 
     Parameters:
@@ -24,6 +23,10 @@ def inflate_string(input:str, max_length:int|None=None) -> str:
     Returns:
         string
     '''
+    #force trim. its ur fault if you try to fill the whole char limit
+    if (max_length != None):
+        input = force_trim(input, max_length)
+    
     skipper = 0
     # i know little about python but i hate these for loops and
     # don't have time to learn how to properly use them.
@@ -33,16 +36,19 @@ def inflate_string(input:str, max_length:int|None=None) -> str:
             input = input[:i]+"'"+input[i:]
             skipper+=1
     
-    #force trim. its ur fault if you fill your name with apostrophes anyway
-    if (max_length != None and len(input) > max_length):
-        input = input[:max_length]
-        #just in case i cut off a double single quote
-        if (max_length > 1 and input[max_length-1] == "'"):
-            apostropheCounter = 0
-            for i in range(len(input)):
-                if input[i] == "'": apostropheCounter += 1
-            if (apostropheCounter % 2 == 1):
-                input = input[:max_length-1]
-    
     return input
 
+def force_trim(input:str, max_length:int) -> str:
+    '''
+    Limit the length of a string in case of middle school hackers
+
+    Parameters:
+        input (str): The string to be trimmed
+        max_length (int): Maximum size of the string in len(). e.g. Counting index 0 as length 1, index 1 as 2, etc.
+
+    Returns:
+        string
+    '''
+    if (len(input) > max_length):
+        input = input[:max_length]
+    return input
