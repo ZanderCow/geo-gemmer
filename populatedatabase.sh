9327cd9f2c0e9a948820dbe0c338962fab9e7472
Zander Cowan <128069231+ZanderCow@users.noreply.github.com>
2024-05-02 15:28:40 -0400

    added bash command to populate database

---
+++ populatedatabase.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is a bash script to run a Python script
+
+# Activate your Python environment if needed
+# source /path/to/your/venv/bin/activate
+
+# Run your Python script
+python "populate_database.py"
